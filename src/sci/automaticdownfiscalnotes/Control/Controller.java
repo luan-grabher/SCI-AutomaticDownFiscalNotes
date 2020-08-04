a9c8bde40400fc00c1df2270c3f60762531d6295
User <User@pc>
2020-08-04 14:22:55 -0300

    Added class Controller

---
+++ src/sci/automaticdownfiscalnotes/Control/Controller.java
@@ -0,0 +1,5 @@
+package sci.automaticdownfiscalnotes.Control;
+
+public class Controller {
+    
+}
--- src/sci/automaticdownfiscalnotes/SCIAutomaticDownFiscalNotes.java
+++ src/sci/automaticdownfiscalnotes/SCIAutomaticDownFiscalNotes.java
@@ -1,8 +1,18 @@
 package sci.automaticdownfiscalnotes;
 
+import java.io.File;
+import javax.swing.JOptionPane;
+
 public class SCIAutomaticDownFiscalNotes {
     
     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Selecione o arquivo de Recebimentos com Retenção do plune com as baixas a serem feitas:");
+        File file = Selector.Arquivo.selecionar(System.getProperty("user.home"), "XLSX", "xlsx");
         
-    }    
+        execute(file);
+    }
+    
+    public static void execute(File file){
+    
+    }
 }
