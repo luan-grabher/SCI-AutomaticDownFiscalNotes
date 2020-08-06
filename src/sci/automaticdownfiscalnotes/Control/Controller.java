f74a045740f606c51be321a63f86b4ed2098bccc
User <User@pc>
2020-08-06 15:48:08 -0300

    Changed name of env file. Added call to Executor in main function. Added method to save the log in desktop.

--- src/sci/automaticdownfiscalnotes/Control/Controller.java
+++ src/sci/automaticdownfiscalnotes/Control/Controller.java
@@ -9,13 +9,14 @@ import sci.automaticdownfiscalnotes.Model.DownImportationModel;
 import sql.Database;
 
 public class Controller {
+
     public Integer onlineConferenceKey = Integer.valueOf(Env.get("onlineConferenceKey"));
     public Integer onlinePlan = Integer.valueOf(Env.get("onlinePlan"));
     public Integer downType = Integer.valueOf(Env.get("downType"));
     public Integer enterpriseCode = Integer.valueOf(Env.get("enterpriseCode"));
-    
+
     private File downFile;
-    
+
     //Models
     private DownFileModel downFileModel = new DownFileModel();
     private DownImportationModel downImportationModel = new DownImportationModel();
@@ -26,9 +27,9 @@ public class Controller {
 
     public void setDownFile(File downFile) {
         this.downFile = downFile;
-    }        
-    
-    public class connectToDatabase extends Executavel{
+    }
+
+    public class connectToDatabase extends Executavel {
 
         public connectToDatabase() {
             name = "Conectando ao banco de dados...";
@@ -37,15 +38,14 @@ public class Controller {
         @Override
         public void run() {
             Database.setStaticObject(new Database(FileManager.getFile(Env.get("databaseFilePath"))));
-            if(!Database.getDatabase().testConnection()){
+            if (!Database.getDatabase().testConnection()) {
                 throw new Error("Erro ao conectar ao banco de dados.");
             }
         }
-        
-        
+
     }
-    
-    public class setDownFile extends Executavel{
+
+    public class setDownFile extends Executavel {
 
         public setDownFile() {
             name = "Definindo o arquivo do modelo de baixas.";
@@ -55,13 +55,13 @@ public class Controller {
         public void run() {
             downFileModel.setFile(downFile);
             downFileModel.setDowns();
-            
+
             downImportationModel.setDowns(downFileModel.getDowns());
         }
-        
+
     }
-    
-    public class importDowns extends Executavel{
+
+    public class importDowns extends Executavel {
 
         public importDowns() {
             name = "Importando baixas";
@@ -70,6 +70,24 @@ public class Controller {
         @Override
         public void run() {
             downImportationModel.importDowns();
-        }        
+        }
+    }
+
+    public class saveLog extends Executavel {
+
+        public saveLog() {
+            name = "Salvando Log";
+        }
+
+        @Override
+        public void run() {
+            FileManager.save(
+                    new File(
+                            System.getProperty("user.home")
+                            + "\\desktop\\LOG Baixar Notas.txt"),
+                     downImportationModel.getLog().toString()
+            );
+        }
+
     }
 }
--- src/sci/automaticdownfiscalnotes/SCIAutomaticDownFiscalNotes.java
+++ src/sci/automaticdownfiscalnotes/SCIAutomaticDownFiscalNotes.java
@@ -1,24 +1,44 @@
 package sci.automaticdownfiscalnotes;
 
+import Entity.Executavel;
+import Executor.Execution;
+import SimpleDotEnv.Env;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 import sci.automaticdownfiscalnotes.Control.Controller;
 
 public class SCIAutomaticDownFiscalNotes {
-    
+
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Selecione o arquivo de Recebimentos com Retenção do plune com as baixas a serem feitas:");
         File file = Selector.Arquivo.selecionar(System.getProperty("user.home"), "XLS", "xls");
-        
+
         execute(file);
     }
-    
-    public static void execute(File file){
-        Controller controller =  new Controller();
-        controller.setDownFile(file);
-        
-        controller.new connectToDatabase().run();
-        controller.new setDownFile().run();
-        controller.new importDowns().run();
+
+    public static void execute(File file) {
+        try {
+            Env.setPath("AutomaticDownFiscalNotes");
+
+            Controller controller = new Controller();
+            controller.setDownFile(file);
+            
+            List<Executavel> execs = new ArrayList<>();
+
+            execs.add(controller.new connectToDatabase());
+            execs.add(controller.new setDownFile());
+            execs.add(controller.new importDowns());
+            execs.add(controller.new saveLog());
+            
+            Execution execution =  new Execution("Baixar notas " + file.getName());
+            execution.setExecutables(execs);
+            execution.runExecutables();
+            execution.endExecution();            
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage(), "Ocorreu um erro!", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
